f2118a1d8d6750a78c2c427880524062f6a7818c
Sipho-sovuyo Manxoyi <siphosovuyomanxoyi@gmail.com>
2025-11-07 16:36:47 +0200

    SM - Initial commit for payments project

---
+++ src/main/java/com/sipho/za_payments_core/ZaPaymentsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sipho.za_payments_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaPaymentsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaPaymentsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipho/za_payments_core/ZaPaymentsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipho.za_payments_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaPaymentsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
